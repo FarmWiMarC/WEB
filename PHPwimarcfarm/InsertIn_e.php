049a3e2834b3ffaae037c4b6bd7a4ebb2bf961a9
Montree <30278208+WiMarCFarmIoT@users.noreply.github.com>
2017-08-27 12:35:44 +0700

    Add files via upload

---
+++ PHPwimarcfarm/InsertIn_e.php
@@ -0,0 +1,47 @@
+<?php
+        
+        
+     header('Content-type: text/html');
+    header('Access-Control-Allow-Origin: *');
+     // $uri = 'http'. ($_SERVER['HTTPS'] ? 's' : null) .'://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+   // echo('<p>This information has come from <a href="' . $uri . '">' . $uri . '</a></p>');
+$uri = 'http'. (isset($_SERVER['HTTPS']) ? 's' : null) .'://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];    
+        
+        
+  $request_method = $_SERVER['REQUEST_METHOD'];
+ 
+  switch($request_method) {
+    case 'PUT':
+    case 'DELETE':
+      // Ignore the PUT and DELETE request =P
+      return;
+    case 'GET':
+       echo "aacc";// GET request received
+      break;
+    case 'POST':
+     	$date=$_POST['date'];
+        	$time=$_POST['time'];
+                	$A=$_POST['A'];
+                        $B=$_POST['B'];
+                        $C=$_POST['C'];
+                        $D=$_POST['D'];
+                         $E=$_POST['E'];
+                        $F=$_POST['F'];
+                        $G=$_POST['G'];
+                        $H=$_POST['H'];
+       // echo "result is $date $time $A $B $C $D"; 
+      include "dblink.php";
+
+
+	$sql = "Insert into e values('$date', '$time', '$A','$B', '$C', '$D', '$E','$F', '$G', '$H');";
+       
+	if ($conn->query($sql) === TRUE) {
+echo "New record created successfully";
+} else {
+echo "Error: " . $sql . "<br>" . $conn->error;
+}$conn->close();
+        
+      echo " POST in database ";// POST request received
+      break;
+  }
+?>
---
+++ PHPwimarcfarm/dblink.php
@@ -0,0 +1,6 @@
+<?php
+ 
+ $conn = mysqli_connect("localhost","db-user","password","db-name");
+
+ // $sql = "use id2613481_sensora";
+?>
